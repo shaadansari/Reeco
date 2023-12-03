de7e6a392d9cec123c5f671ec7162def3d1ff676
shaadansari <ansarisaad90@gmail.com>
2023-12-03 21:40:32 +0530

    setup custom hook for API Calling

---
+++ src/customHook/useAxios.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8000/orderData";
+
+const useAxios = ({ url, method, body = null, headers = null }) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = () => {
+    axios[method](url, JSON.parse(headers), JSON.parse(body))
+      .then((res) => {
+        setResponse(res.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [method, url, body, headers]);
+
+  return { response, error, loading };
+};
+
+export default useAxios;
