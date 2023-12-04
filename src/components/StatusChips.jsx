cdb37e8170153187d7d5ab122bb82742c9382e39
shaadansari <ansarisaad90@gmail.com>
2023-12-04 12:35:18 +0530

    update status text

--- src/components/StatusChips.jsx
+++ src/components/StatusChips.jsx
@@ -1,8 +1,10 @@
 import Chip from "@mui/material/Chip";
 
+const statusText = {success:"Approved", warning:"Missing - Urgent", info:"Missing"}
+
 // eslint-disable-next-line react/prop-types
 function StatusChips({status}) {
-  return <Chip label="primary" color={status} />;
+  return <Chip label={statusText[status]} color={status} />;
 }
 
 export default StatusChips;
